6baa1d7053379639e9a041d56b4ddcb6071c3c2b
Jan Mercl <jnml@fsc-r550.(none)>
2013-08-22 14:31:03 +0200

    go: WIP snapshot

---
+++ go/etc.go
@@ -0,0 +1,11 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package parser
+
+//TODO idlist_colas	= . // identifier { "," identifier } colas .
+//TODO identifier_list = . // Manually enabled in proper contexts.
+//TODO pack_name	= . // Including the following ".".
+//TODO built_in_name	= . //ocomma_rparen: xlat "," ")" -> ")" and "," "}" -> "}"; check legality post hoc.
+//TODO lbr = .
