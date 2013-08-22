d878480c2dc1bbd213f587b3266d5433cb60f19f
Jan Mercl <jnml@fsc-r550.(none)>
2013-08-22 16:15:05 +0200

    go: Milestone: Name kind hints required no more

--- go/etc.go
+++ go/etc.go
@@ -6,6 +6,4 @@ package parser
 
 //TODO idlist_colas	= . // identifier { "," identifier } colas .
 //TODO identifier_list = . // Manually enabled in proper contexts.
-//TODO pack_name	= . // Including the following ".".
-//TODO built_in_name	= . //ocomma_rparen: xlat "," ")" -> ")" and "," "}" -> "}"; check legality post hoc.
 //TODO lbr = .
