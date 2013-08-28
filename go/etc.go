d8b5c067d1df2f8b0054168e875362bf44d920f9
Jan Mercl <jnml@fsc-r550.(none)>
2013-08-28 13:37:07 +0200

    go: WIP snapshot (dbg) fsm23

--- go/etc.go
+++ go/etc.go
@@ -250,7 +250,8 @@ dump:
 		case st2:
 			switch r {
 			case '(':
-				panic("st2 (")
+				x.toks, x.ids, x.state = append(x.toks, tk), nil, st3
+				x.preamble = len(x.toks)
 			case IDENTIFIER:
 				x.preamble = len(x.toks)
 				x.toks, x.ids, x.state = append(x.toks, tk), []tok{tk}, st4
@@ -262,7 +263,8 @@ dump:
 			case IDENTIFIER:
 				x.toks, x.ids, x.state = append(x.toks, tk), append(x.ids, tk), st4
 			default:
-				panic("st3 default")
+				x.dump, x.state = append(x.toks, tk), st1
+				goto dump
 			}
 		case st4: // state 4 accepts rule 1
 			switch r {
