200f90fe7e5463f55e9426f5db458da51a4bdbaf
Jan Mercl <jnml@fsc-r550.(none)>
2013-09-06 14:00:54 +0200

    go: WIP be4 introducing dot_lpar

--- go/etc.go
+++ go/etc.go
@@ -359,9 +359,8 @@ dump:
 				x.preamble, x.toks, x.ids, x.state = -1, append(x.toks, tk), nil, st9
 			case ',':
 				x.toks, x.state = append(x.toks, tk), st11
-
 			default:
-				panic("st13 default")
+				x.dump = append(x.toks[:x.preamble], tok{IDENTIFIER_LIST, x.ids, x.ids[0].pos}, tk)
 			}
 		case st14:
 			switch r {
