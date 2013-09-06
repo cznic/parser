7793c0074012b706555d5975e558c382ce203fc8
Jan Mercl <jnml@fsc-r550.(none)>
2013-09-06 15:13:42 +0200

    go: WIP snapshot (dbg,fsm19)

--- go/testdata/identifier_list.go
+++ go/testdata/identifier_list.go
@@ -136,61 +136,61 @@ const (
 	g    = 6
 	h, i = 7, 8
 )
-//const ( j = 1; k, l = 9, 10 )
-//
-//const ()
-//const (
-//	e = 4;
-//)
-//const (	f = 5; )
-//const (
-//	g    = 6;
-//	h, i = 7, 8;
-//)
-//const ( j = 1; k, l = 9, 10; )
-//
-//var m = 11
-//var n, o = 12, 13
-//var ()
-//var (
-//	p = 14
-//)
-//var ( q = 15 )
-//var (
-//	r    = 16
-//	s, t = 17, 18
-//)
-//var ( u = 19; v, w = 19, 20 )
-//
-//var ()
-//var (
-//	p = 14;
-//)
-//var ( q = 15; )
-//var (
-//	r    = 16;
-//	s, t = 17, 18;
-//)
-//var ( u = 19; v, w = 19, 20; )
-//
-//type ()
-//type (
-//	t struct{}
-//	t struct { *a }
-//	t struct { *b.c }
-//	t struct { d }
-//	t struct { e.f }
-//	t struct { g int }
-//	t struct { h, i int }
-//
-//	t struct { *a; *b }
-//	t struct { *c.d; *e.f }
-//	t struct { g; h }
-//	t struct { i.j; k.l }
-//	t struct { m int; n int }
-//	t struct { o, p int; q, r int }
-//)
-//
+const ( j = 1; k, l = 9, 10 )
+
+const ()
+const (
+	e = 4;
+)
+const (	f = 5; )
+const (
+	g    = 6;
+	h, i = 7, 8;
+)
+const ( j = 1; k, l = 9, 10; )
+
+var m = 11
+var n, o = 12, 13
+var ()
+var (
+	p = 14
+)
+var ( q = 15 )
+var (
+	r    = 16
+	s, t = 17, 18
+)
+var ( u = 19; v, w = 19, 20 )
+
+var ()
+var (
+	p = 14;
+)
+var ( q = 15; )
+var (
+	r    = 16;
+	s, t = 17, 18;
+)
+var ( u = 19; v, w = 19, 20; )
+
+type ()
+type (
+	//t struct{}
+	//t struct { *a }
+	//t struct { *b.c }
+	//t struct { d }
+	//t struct { e.f }
+	//t struct { g int }
+	//t struct { h, i int }
+
+	//t struct { *a; *b }
+	//t struct { *c.d; *e.f }
+	//t struct { g; h }
+	//t struct { i.j; k.l }
+	//t struct { m int; n int }
+	//t struct { o, p int; q, r int }
+)
+
 //type ()
 //type (
 //	t struct{}
