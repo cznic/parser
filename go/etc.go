39e868def659eaf26289f0cab7dc86a5fa406dfe
Jan Mercl <jnml@fsc-r550.(none)>
2013-09-06 14:09:11 +0200

    go: WIP snapshot (dbg,fsm19)

--- go/etc.go
+++ go/etc.go
@@ -295,7 +295,7 @@ dump:
 			case '(':
 				x.toks, x.state = append(x.toks, tk), st6
 			case IDENTIFIER:
-				panic("st5 identifier")
+				x.toks, x.state = append(x.toks, tk), st10
 			default:
 				panic("st5 default")
 			}
@@ -332,7 +332,12 @@ dump:
 				x.dump = append(x.toks, tk)
 			}
 		case st10:
-			panic(fmt.Sprintf("TODO st%d", x.state+1))
+			switch r {
+			case '(':
+				x.toks, x.state = append(x.toks, tk), st11
+			default:
+				panic("st10 default")
+			}
 		case st11:
 			switch r {
 			case IDENTIFIER:
