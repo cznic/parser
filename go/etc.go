15790ae47f71c640529752de0e0302f7f0239832
Jan Mercl <jnml@fsc-r550.(none)>
2013-09-06 14:12:09 +0200

    go: WIP snapshot (dbg,fsm19)

--- go/etc.go
+++ go/etc.go
@@ -324,7 +324,7 @@ dump:
 		case st9:
 			switch r {
 			case IDENTIFIER:
-				panic("st9 identifier")
+				x.rxFix, x.preamble, x.toks, x.state = -1, -1, append(x.toks, tk), st10
 			default:
 				if i := x.rxFix; i >= 0 {
 					x.toks[i] = tok{IDENTIFIER_LIST, x.ids, x.ids[0].pos}
