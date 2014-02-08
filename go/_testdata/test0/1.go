701eac500d70b076d6c4fbc2f33bce7c0edb28ae
Jan Mercl <0xjnml@gmail.com>
2014-02-08 23:14:19 +0100

    go: WIP snapshot: ok

--- go/_testdata/test0/1.go
+++ go/_testdata/test0/1.go
@@ -36,3 +36,31 @@ const (
 	Partyday36
 	numberOfDays37 // this constant is not exported
 )
+const (  // iota is reset to 0
+	c040 = iota  // c0 == 0
+	c141 = iota  // c1 == 1
+	c242 = iota  // c2 == 2
+)
+
+//const (
+//	a46 = 1 << iota  // a == 1 (iota has been reset)
+//	b47 = 1 << iota  // b == 2
+//	c48 = 1 << iota  // c == 4
+//)
+//
+//const (
+//	u52         = iota * 42  // u == 0     (untyped integer constant)
+//	v53 float64 = iota * 42  // v == 42.0  (float64 constant)
+//	w54         = iota * 42  // w == 84    (untyped integer constant)
+//)
+//
+//const x57 = iota  // x == 0 (iota has been reset)
+//const y58 = iota  // y == 0 (iota has been reset)
+//Within an ExpressionList, the value of each iota is the same because it is only incremented after each ConstSpec:
+//
+//const (
+//	bit062, mask062 = 1 << iota, 1<<iota - 1  // bit0 == 1, mask0 == 0
+//	bit163, mask163                           // bit1 == 2, mask1 == 1
+//	_, _                                  // skips iota == 2
+//	bit365, mask365                           // bit3 == 8, mask3 == 7
+//)
